36cec6215e5025a67e0652bb1c6c56d0d58de620
Francois Supervielle-Brouques <francoisup40@gmail.com>
2023-04-12 10:53:29 +0200

    add button & qty input OK

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-/* import { AppRoutingModule } from './app-routing.module'; */
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { TrainingsComponent } from './components/trainings/trainings.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -13,8 +13,8 @@ import { TrainingsComponent } from './components/trainings/trainings.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-    /* AppRoutingModule */
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/trainings/trainings.component.ts
+++ src/app/components/trainings/trainings.component.ts
@@ -11,14 +11,16 @@ export class TrainingsComponent implements OnInit {
   constructor(){ }
 
   ngOnInit(): void {
-    
+    this.listTrainings = [
+      {id:1,name:'JAVA',description:'formation Java SE 8 sur 5 jours',price:1500,quantity:0},
+      {id:2,name:'DotNet',description:'formation DotNet sur 3 jours',price:1000,quantity:0},
+      {id:3,name:'Python',description:'formation Python sur 5 jours',price:1500,quantity:0},
+    ]
   }
 
-  displayTrainings() {
-    this.listTrainings = [
-      {id:1,name:'JAVA',description:'formation Java SE 8 sur 5 jours',price:1500},
-      {id:2,name:'DotNet',description:'formation DotNet sur 3 jours',price:1000},
-      {id:3,name:'Python',description:'formation Python sur 5 jours',price:1500},
-    ]
+  onAddToCart(training:Training) {
+    console.log(training.name)
+    console.log(training.quantity)
+  
   }
 }
